1dd803c88d8f5440aceb73cb55c25fde9a3a3eaa
henriquepablo <pablo269704@gmail.com>
2024-10-23 19:38:39 -0300

    chore: adjusting the message that the user receive

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://172.16.1.253:8000/api/v1/'
+    baseURL: 'http://192.168.26.223:8000/api/v1/'
 });
 
 export default api;
